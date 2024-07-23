79c77cbd15ba6891992a5cbc5b0592c2577f6a1c
Duc Nguyen <49058540+Duc-Nguyen-2101@users.noreply.github.com>
2024-07-23 17:40:08 +0700

    Update solutions
    update solutions

--- Leetcode_Problems/Sort_the_people.py
+++ Leetcode_Problems/Sort_the_people.py
@@ -1,19 +1,26 @@
 class Solution:
     def sortPeople(self, names: list[str], heights: list[int]) -> list[str]:
-        n: int = len(names)
-        mapping: dict[int, str] = {}  # height -> name (heights are distinct)
-        for ind in range(n):
-            mapping[heights[ind]] = names[ind]
-
-        heights.sort(reverse=True)
-        for ind in range(n):
-            h: int = heights[ind]
-            names[ind] = mapping[h]
-
-        return names
+        n:int = len(heights)
+        for i in range(n):
+            for j in range(i, n-1):
+                if heights[i] > heights[j]:
+                    # names[i], names[j] = names[i], names[j]
+                    heights[i], heights[j] = heights[i], heights[j]
+                else:
+                    # names[i], names[j] = names[j], names[i]
+                    heights[i], heights[j] = heights[j], heights[i]
+        
+        return heights
     
+# names = ["IEO","Sgizfdfrims","QTASHKQ","Vk","RPJOFYZUBFSIYp","EPCFFt","VOYGWWNCf","WSpmqvb"]
+# names = ["1","2","3","4","5","6","7","8"]
+# heights = [17233,32521,14087,42738,46669,65662,43204,8224]
+
 names = ["Mary","John","Emma"]
 heights = [180,165,170]
 
 Test = Solution()
+
+# Expected ["EPCFFt","RPJOFYZUBFSIYp","VOYGWWNCf","Vk","Sgizfdfrims","IEO","QTASHKQ","WSpmqvb"]
+# Expected ["6", "5", "7", "4", "2", "1", "3", "8"]
 print(Test.sortPeople(names, heights))
--- Practices/main.py
+++ Practices/main.py
@@ -1,17 +1,40 @@
 class Solution:
-    def sortPeople(self, names: list[str], heights: list[int]) -> list[str]:
-        n : int = len(heights)
-        mapping : dict[int, str] = {}
-        for i in range(n):
-            mapping[heights[i]] = names[i]
+    # def sortPeople(self, names: list[str], heights: list[int]) -> list[str]:
+        # n : int = len(heights)
+        # # Create a dictionary to store the height and name
+        # mapping : dict[int, str] = {}
 
-        # Using list.sort(reverse=False/True) to sort all elements in
-        # the list in order (from smaller to bigger or versa)
-        # In this case use True because we want sort name who has highest
-        # height
+        # # Loop through the list of names and heights
+        # for i in range(n):
+        #     mapping[heights[i]] = names[i]
+
+        # # Using list.sort(reverse=False/True) to sort all elements in
+        # # the list in order (from smaller to bigger or versa)
+        # # In this case use True because we want sort name who has highest
+        # # height
+        # heights.sort(reverse=True)
+
+        # # Loop through the list of heights
+        # # Dictionary mapping can be accessed by using syntax
+        # # mapping[heihgts[index]] => return the name of the person who has
+        # for i in range(n):
+        #     names[i] = mapping[heights[i]]
+            
+        # return names
+
+
+    def sortPeople(self, names: list[str], heights: list[int]) -> list[str]:
+        n:int = len(heights)
+        for i in range(n):
+            for j in range(0, n-1-i):
+                if heights[i] > heights[j]:
+                    names[i], names[j] = names[i], names[j]
+                    # heights[i], heights[j] = heights[i], heights[j]
+                else:
+                    names[i], names[j] = names[j], names[i]
+                    # heights[i], heights[j] = heights[j], heights[i]
         
-        heights.sort(reverse=True)
-        return heights
+        return names
     
 names = ["Mary","John","Emma"]
 heights = [180,165,170]
