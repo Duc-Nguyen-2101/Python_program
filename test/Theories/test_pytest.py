32d84cb8a88de2b40f320d3839c4efa0a782d29c
Duc Nguyen <49058540+Duc-Nguyen-2101@users.noreply.github.com>
2024-08-05 17:52:54 +0700

    Update files

--- test/Theories/test_pytest.py
+++ test/Theories/test_pytest.py
@@ -1,8 +1,9 @@
+import pytest
 from src.Theories.Python_practices import *
 
 def test_increment():
     assert increment(3) == 4
 
-def test_sort_function():
-    name = input()
-    assert sort_function(name)
+# def test_sort_function():
+#     name = input()
+#     assert sort_function(name)
